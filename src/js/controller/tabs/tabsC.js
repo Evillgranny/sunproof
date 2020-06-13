8e83e803c09c00f0bdfc4560cca7dc98516b158c
Evillgranny <tractor2554393@yandex.by>
2020-06-13 17:48:46 +0300

    add glazing page

--- src/js/controller/tabs/tabsC.js
+++ src/js/controller/tabs/tabsC.js
@@ -1,10 +1,12 @@
 import { Tabs } from "../../model/Tabs/Tabs";
-import { allTabsMarquises, allContainersMarquises, allTabsPergolas, allContainersPergolas } from "../../view/tabsV/tabsV";
+import { allTabsMarquises, allContainersMarquises, allTabsPergolas, allContainersPergolas, allTabsGlazing, allContainersGlazing } from "../../view/tabsV/tabsV";
 
 let tabsMarquises = new Tabs(allTabsMarquises, allContainersMarquises)
 let tabsPePergolas = new Tabs(allTabsPergolas, allContainersPergolas)
+let tabsGlazing = new Tabs(allTabsGlazing, allContainersGlazing)
 
 tabsMarquises.tabClick()
 tabsPePergolas.tabClick()
+tabsGlazing.tabClick()
 
-export { tabsMarquises, tabsPePergolas }
+export { tabsMarquises, tabsPePergolas, tabsGlazing }
--- src/js/view/tabsV/tabsV.js
+++ src/js/view/tabsV/tabsV.js
@@ -1,8 +1,14 @@
 let allTabsMarquises = document.querySelectorAll('.tabs-navigation__tab[data-navigation="marquises"]')
 let allContainersMarquises = document.querySelectorAll('.tabs-container[data-navigation="marquises"]')
+
 let allTabsPergolas = document.querySelectorAll('.tabs-navigation__tab[data-navigation="pergolas"]')
 let allContainersPergolas = document.querySelectorAll('.tabs-container[data-navigation="pergolas"]')
 
+let allTabsGlazing = document.querySelectorAll('.tabs-navigation__tab[data-navigation="glazing"]')
+let allContainersGlazing = document.querySelectorAll('.tabs-container[data-navigation="glazing"]')
 
 
-export { allTabsMarquises, allContainersMarquises, allTabsPergolas, allContainersPergolas }
+
+export { allTabsMarquises, allContainersMarquises,
+    allTabsPergolas, allContainersPergolas,
+    allTabsGlazing, allContainersGlazing }
